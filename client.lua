23725260d2cf634a1d1b2bc03f64c428de63450c
Manvaril <razor.ps3.mad@gmail.com>
2020-11-04 15:43:15 -0600

    Auto Seat Shuffle
    Disable the automatic seat shuffle from passenger seat to drviers

--- client.lua
+++ client.lua
@@ -12,6 +12,13 @@ Citizen.CreateThread(function()
 				openVehControl()
 			end
 		end
+		if IsPedInAnyVehicle(GetPlayerPed(-1), false) and DisableSeatShuffle then
+			if GetPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), false), 0) == GetPlayerPed(-1) then
+				if GetIsTaskActive(GetPlayerPed(-1), 165) then
+					SetPedIntoVehicle(GetPlayerPed(-1), GetVehiclePedIsIn(GetPlayerPed(-1), false), 0)
+				end
+			end
+		end
     end
 end)
 
--- config.lua
+++ config.lua
@@ -1,1 +1,5 @@
+-- Commands [true/false]
 UseCommands = true
+
+-- Disable ped moving seat from pass to driver unintentionally [true/false]
+DisableSeatShuffle = true

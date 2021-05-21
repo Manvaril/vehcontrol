cf72626601a1c4c74da302dd3164f22f976a65ca
Manvaril <razor.ps3.mad@gmail.com>
2021-05-20 21:08:14 -0500

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 author 'Manvaril'
 description 'Vehicle Door/Window/Seat/Engine/Dome Light NUI script'
-version '1.1.3b'
+version '1.1.3'
 
 ui_page "html/vehui.html"
 
